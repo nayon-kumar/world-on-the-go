31782777b7aec65a3d404c8e09f732c53f6a31b5
nayon-kumar <nayon.cse55@gmail.com>
2026-03-20 19:39:01 +0600

    Using font awesome icon

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,15 @@
-import { Suspense } from "react";
-import "./App.css";
-import Navbar from "./components/Navbar/Navbar";
-import PricingOptions from "./components/PricingOptions/PricingOptions";
-import ResultChart from "./components/ResultChart/ResultChart";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHeart } from "@fortawesome/free-solid-svg-icons";
 
-const pricingPromise = fetch("pricingData.json").then((res) => res.json());
-const marksPromise = fetch("marks.json").then((res) => res.json());
-
-function App() {
+const App = () => {
   return (
-    <>
-      <header>
-        <Navbar></Navbar>
-      </header>
-      <main>
-        <Suspense
-          fallback={
-            <div className="flex items-center justify-center mt-30">
-              <span className="loading loading-spinner loading-xl"></span>
-            </div>
-          }
-        >
-          <PricingOptions pricingPromise={pricingPromise}></PricingOptions>
-        </Suspense>
-        <Suspense
-          fallback={
-            <div className="flex items-center justify-center mt-30">
-              <span className="loading loading-spinner loading-xl"></span>
-            </div>
-          }
-        >
-          <ResultChart marksPromise={marksPromise}></ResultChart>
-        </Suspense>
-      </main>
-      <footer></footer>
-    </>
+    <div>
+      <h1>
+        <FontAwesomeIcon icon={faHeart} />
+        Hello!
+      </h1>
+    </div>
   );
-}
+};
 
 export default App;
